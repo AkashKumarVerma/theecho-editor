444d957b0652f3fd1874b3306038213a9544e308
Akash Kumar Verma <akashkumar121212@gmail.com>
2019-03-11 11:51:12 +0530

    remove service worker for now

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import './registerServiceWorker'
+// import './registerServiceWorker'
 import ApiService from '@/services/api.service'
 import config from '@/config'
 
