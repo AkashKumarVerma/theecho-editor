8d0bf5f551520a5165731fdb359da416593070c0
Akash Kumar Verma <akashkumar121212@gmail.com>
2019-03-11 11:17:44 +0530

    Changes api rl from dev to prod

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import './registerServiceWorker'
 import ApiService from '@/services/api.service'
 import config from '@/config'
 
-ApiService.init('http://localhost:3001/api/editor')
+ApiService.init('https://api.theecho.in/api/editor')
 
 Vue.config.productionTip = false
 
